d497600a5762b82ae660fbe226de7256fbc4bcb1
mirza-sync <mirzasyahmi123@gmail.com>
2023-08-31 23:57:56 +0800

    Get employees list on init modal

--- src/app/components/department/create-department-modal/create-department-modal.component.ts
+++ src/app/components/department/create-department-modal/create-department-modal.component.ts
@@ -1,22 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { DepartmentService } from 'src/app/services/department.service';
-import { Department } from 'src/app/types/types';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { Department, Employee } from 'src/app/types/types';
 
 @Component({
   selector: 'app-create-department-modal',
   templateUrl: './create-department-modal.component.html',
   styleUrls: ['./create-department-modal.component.css'],
 })
-export class CreateDepartmentModalComponent {
-  @Input('department') department: Department | undefined;
+export class CreateDepartmentModalComponent implements OnInit {
+  @Input('department') department: Department = {} as Department;
   name: string = '';
   deptEmployeeSelection = [];
+  employees: Employee[] = [];
+  employeeNotThisDeparment: Employee[] = [];
 
   constructor(
     private modalService: BsModalService,
-    private departmentService: DepartmentService
+    private departmentService: DepartmentService,
+    private employeeService: EmployeeService
   ) {}
+
+  ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (res) => {
+        this.employees = res;
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+  }
 
   addDepartment() {
     const payload = {
